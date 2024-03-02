721546b9e631b811e467de97c90d8a6ff0eeb803
Emilio-115 <emivazcru@alum.us.es>
2024-03-02 15:41:08 +0100

    Tests pasados

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -22,19 +22,68 @@ const index = async function (req, res) {
 // TODO: Complete the following functions
 
 const create = async function (req, res) {
+  const newRestaurant = Restaurant.build(req.body)
+  newRestaurant.userId = 1
 
+  try {
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const show = async function (req, res) {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }
+      ],
+      order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+    }
+    )
 
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const update = async function (req, res) {
+  try {
+    const restaurantExist = await Restaurant.findByPk(req.params.restaurantId)
+    if (!restaurantExist) {
+      res.status(404).send('No existe el restaurante.')
+    }
 
+    const restaurant = await restaurantExist.update(req.body)
+
+    const restauranteUpd = await restaurant.save()
+    res.json(restauranteUpd)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
-
+  const destr = await Restaurant.destroy({
+    where: {
+      id: req.params.restaurantId
+    }
+  })
+  try {
+    res.send('Eliminadas' + destr + 'filas')
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const RestaurantController = {
